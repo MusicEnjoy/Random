4fcb6f90df5bcfef240e5bc9901a736b12cf4a7f
MusicEnjoy <115658647+MusicEnjoy@users.noreply.github.com>
2023-03-04 16:30:59 +0700

    Add files via upload

---
+++ List.js
@@ -0,0 +1,223 @@
+let allMusic = [
+  {
+    id: "song-1",
+    name: "Darah Muda",
+    artist: "Rhoma Irama",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjdU_T5sKLvXLCfyFC--DwZNtWq5_5Tm3qj0RcLHeiMj8kF3ArCIk_178Dm-n2ncaiiRux1BwYy0Vm49-nuHqnAdxnQsHPTQgg3d_qoQGGrk4KMWFYrUDsYFYePPaNjoLpisl_E47tYmO2vyA-ru1t3o4ArJlJbeAGnA9_sTm82eNeXUhe-xwLsImuwDw/s320/darah%20muda.png",
+    src: "https://github.com/STORAGESHOP/music/raw/main/Rhoma%20Irama%20-%20Darah%20Muda%20(Official%20Lyric%20Video).mp3"
+  },
+  
+
+  
+//SPASI  
+  
+
+  
+  {
+    id: "song-2",
+    name: "Lily-Alan Walker, K-391 & Emelie Hollow",
+    artist: "Alan Walker",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhN91iVN3OzbMG5LxQHuSU6YlnoXX_KGs8voBnOCoYqtStu3URGvqqITepVq_kTnMyOaU0wMQQgLt5JO-Iq4_9GmWFZg4pCQb1Dm2_LqfjqmWgWlhLkiHhoTp8a7jVi9Kx8J9PC-Env9SvLCEOcfHMSlRo9s9fxGkqroN3dx6WDx-_kCwanEy_y6zjAfg/s320/Lily.png",
+    src: "https://github.com/STORAGESHOP/music/raw/main/Alan%20Walker%2C%20K-391%20%26%20Emelie%20Hollow%20-%20Lily.mp3"
+  },
+  
+  
+  
+//SPASI  
+  
+  
+
+  {
+    id: "song-3",
+    name: "Bidadari Cinta",
+    artist: "Adella",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgMAvywnEW91pfHsi3Zts5voIyRyXv49ZXIv-dGrL8HeLFa-2UaxbEa9yrwVOLYUjC70tfCTdSlzthzMomdPfcCHusUlAiTHxkkSwbgbG26R_SOA76ghPVwP3hso8PBDs5oJiq-bBhJKOOg15TRv3U3vC2IW0i6HZKWO7pq3xZQb32_R86PQC0NhdyhXg/s320/Bidadari%20Cinta.png",
+    src: "https://github.com/STORAGESHOP/music/raw/main/BIDADARI%20CINTA%20-%20Fendik%20Adella%20feat%20Lusyana%20Jelita%20-%20OM%20ADELLA.mp3"
+  },
+  
+  
+  
+//SPASI  
+  
+  
+
+{
+    id: "song-4",
+    name: "Juragan Empang",
+    artist: "Nella Kharisma",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjLXHYVuGFpYmqK26Hd7-5NrDnEKHXSwngIL5sl6GESe3dS5Ph9e1366-ifGkbGQ7Vgl7AJqpYaCR_oPFoPcdcMK_lySBr2UGGf4tUjxA2xTuHVRG-CEyTtIDmsl-VCeIxe1FCEOyt2mD2_wQQs2lZ6u50SZToM_PUfzqGRP4rLvqpWNYCTs2aDvHUFUQ/s320/Juragan%20Empang.png",
+    src: "https://github.com/STORAGESHOP/music/raw/main/Nella%20Kharisma%20-%20Juragan%20Empang%20%5BOFFICIAL%5D.mp3"
+},
+  
+  
+  
+//SPASI  
+  
+  
+
+{
+    id: "song-5",
+    name: "Runtah Remix Ft Imp ID",
+    artist: "Azmy Z",
+    img: "https://img.youtube.com/vi/lAIdX9Rv3Rk/default.jpg",
+    src: "https://archive.org/download/album_dangdut/RUNTAH%20REMIX%20BY%20AZMY%20Z%20FT%20IMP%20ID%20%23azmyz%20%23runtah%20%23lagusunda.mp3"
+},
+  
+  
+  
+//SPASI  
+  
+
+
+{
+id: "song-6",
+name: "Tiara",
+artist: "Sallsa Bintan 3PB",
+img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiQkmWatytntQFhgLmgMRxh-nJCrKQXc3wNkHsGggHmPiEcaPNjDrrl1gpUuxUsepzidzv4DOmeCIBYOwPJwpL-PUqaWD5h2WOhr5oE7ffERXI60e2BJ8eg5dTsMKwBapvxIz5LrUPwvM4qm_MMjmidGpW2fP3WRPgjMJLSFP4qZ0qrNzi_MV8u2gwjjA/s320/Tiara.png",
+src: "https://github.com/STORAGESHOP/music/raw/main/TIARA%20-%20KRIS%203PB%20SALLSA%20BINTAN.mp3"
+  },
+  
+  
+
+  //SPASI
+
+
+
+{
+ id: "song-7",
+ name: "Sound Gamelan Pernikahan",
+ artist: "Cek Sound",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgQQ2QclcXTewWMklJOdw8ygCo38IRjemoBrwHZmSlYIG7YJAmghw-Nds6NC3fktqawuUAZQLa_wpUWHYeTGztKJS0hk00vL-ITKD5Q34hk21Trij9ffc8pz9Hl4M3b4nA9SxVyKafXTUQUyXYLgziz4l5xwGyQSE0DwiX7Q_vax_PlmevH2bF8o74X7g/s320/GAMELAN.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/Gamelan%20Degung%20-%20Sabi%20Lulungan.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-8",
+ name: "Sound Pernikahan",
+ artist: "Cek Sound",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgwy5ASAFyfR3hm9Cz5kzTcxExO09yoeT-KE-HJnJTwJjylon7VjSPDsegV9eWUz-qlv1bt-l1ZkAtB5WpjASCn5SAxbLw6KNvy8Z9ABwYgl_kQueEf69dVC82bOOitduKA_yYFhhvZF8pU_qKHi4CRppfk9P-E7v6lkLuwen9HbrPFxajHqLTdX0nzqw/s320/sound.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/Sound%20Pernikahan%20Yang%20Di%20Cari-cari%20Di%20Tiktok%20_Virall!!!.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-9",
+ name: "Sound Pernikahan",
+ artist: "Cek Sound",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgwy5ASAFyfR3hm9Cz5kzTcxExO09yoeT-KE-HJnJTwJjylon7VjSPDsegV9eWUz-qlv1bt-l1ZkAtB5WpjASCn5SAxbLw6KNvy8Z9ABwYgl_kQueEf69dVC82bOOitduKA_yYFhhvZF8pU_qKHi4CRppfk9P-E7v6lkLuwen9HbrPFxajHqLTdX0nzqw/s320/sound.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/Cek%20sound%20dangdut%20dua%20lagu%20untuk%20Hajatan%20jenangan.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-10",
+ name: "Dj Gala - Gala Cek Sound",
+ artist: "R2 PROJECT",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi15TUv0-TzgVTSGwOKhmf5f8aCzz-pDEBVrRBe4yZnKNWCiojNtJppqrFALx-rOTDdH297K0Mb1gHNfZWVXS_UPZaa9-hjl3C1sjPymN6NtVYWvk66ZKraeqs5mHDn4ajD57g01S2CkZLacuoZF5HGUTNQ_eEcAfjSoJT3WyHHQ6w1tpQ9PqMVAXKQkQ/s320/GALA%20-%20GALA.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/DJ%20GALA-GALA%20BY%20R2%20PROJECT%20SLOW%20BASS.%20NDEWOR%20AUDIO.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-11",
+ name: "Dj Pong - Pong Cek Sound",
+ artist: "Raditya Sound System Kediri",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiJNDoFS6SxXPmtFG4fQqsO_iEocqJpZD7dg5oNHVEvHOHYmoGKoytzn8PdEDnErXPCM5NZ9RXyeqybKfCmEbCklu_3KbGhAztgi9hXre8u-VhPUViNCs8-xoyak6JNj8gTDx_52bBWY-Xf5Wpf4jGHBFe3yDZDRDB2qoDXcctB7-O4_k_yd1assAr2nw/s320/PONG%20-%20PONG.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/DJ%20PONG%20PONG%20SELOW%20X%20JEDOR%20ENAK%20BUAT%20CEK%20SOUND%20JINGLE%20RADITYA%20PRO%20LIGTING%20%F0%9F%8E%B5%F0%9F%8E%B5.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-12",
+ name: "Dj Slebew Cek Sound",
+ artist: "Dj Alengka - Topik",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgM_DYxofsP5s3Wz-AOnKAPeXdaDgFeB0Ke3uNUMO5jEuqTW7P8Kbiqnicr3YJijI6o24rqZsqV-gwCtss-T0g24vaIhx14mqDE2agkZUHBorNcbBSwkLoMdEpEIHwx8tDmPQ42Rfn2hQ7La8hbUaYz7ck6H3_KvzAO8lkaDOr5fv9XCoYzNG-Fmq0eIQ/s320/Slebew.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/Slebew.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-13",
+ name: "Dj Cek Sound Bass",
+ artist: "Qipli BDL",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgwy5ASAFyfR3hm9Cz5kzTcxExO09yoeT-KE-HJnJTwJjylon7VjSPDsegV9eWUz-qlv1bt-l1ZkAtB5WpjASCn5SAxbLw6KNvy8Z9ABwYgl_kQueEf69dVC82bOOitduKA_yYFhhvZF8pU_qKHi4CRppfk9P-E7v6lkLuwen9HbrPFxajHqLTdX0nzqw/s320/sound.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/DJ%20CEK%20SOUND%20BASS%20GLERR.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-14",
+ name: "Dj Kutidhieng Cek Sound",
+ artist: "Geger Music MultiMedia",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjfskczy0RU39CyEh4wTZlSYCWXy4BS0JR_kaf0iDIVB_0yokCJJrnwOHbRh4vI_FTLJIgG4J2g6kgeE_EjvdfRdfXWLvjiDVGeDs4t18KgHAOmA3qYadNeG_4w0uIeqKTYUyGOjfJ4o-A2gIq50o8cBwlBJrQXBjXYMB1FnPzUvdCm0NhgsCeIiC2GQQ/s320/Kutidhieng.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/%F0%9F%94%B4DJ%20YANG%20DI%20PAKAI%20GEGER%20MUSIC%20CEK%20SOUND%20HOOREEG%20HYDING%20TRAP%20RAGATAK%20TIYOK%20AMK.GEGER%20MUSIC%20PROB.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-15",
+ name: "Dj Kutidhieng Cek Sound",
+ artist: "Goprax Production",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjfskczy0RU39CyEh4wTZlSYCWXy4BS0JR_kaf0iDIVB_0yokCJJrnwOHbRh4vI_FTLJIgG4J2g6kgeE_EjvdfRdfXWLvjiDVGeDs4t18KgHAOmA3qYadNeG_4w0uIeqKTYUyGOjfJ4o-A2gIq50o8cBwlBJrQXBjXYMB1FnPzUvdCm0NhgsCeIiC2GQQ/s320/Kutidhieng.png",
+ src: "https://github.com/STORAGESHOP/cek-sound/raw/main/DJ%20TRAP%20HAIDING%20ALA%20HAIDING%20BANTENGAN%20FULL%20BASS%20GOPRAX%20PRODUCTION.mp3"
+},
+
+
+
+//SPASI
+
+
+
+{
+ id: "song-16",
+ name: "Kutidhieng",
+ artist: "Liza Aulia",
+ img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjfskczy0RU39CyEh4wTZlSYCWXy4BS0JR_kaf0iDIVB_0yokCJJrnwOHbRh4vI_FTLJIgG4J2g6kgeE_EjvdfRdfXWLvjiDVGeDs4t18KgHAOmA3qYadNeG_4w0uIeqKTYUyGOjfJ4o-A2gIq50o8cBwlBJrQXBjXYMB1FnPzUvdCm0NhgsCeIiC2GQQ/s320/Kutidhieng.png",
+ src: "https://github.com/STORAGESHOP/music/raw/main/Liza%20Aulia%20-%20Kutidhieng%20Official%20Music.mp3"
+ },
+  
+  
+  
+  
+];
